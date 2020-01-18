c82914557f481e9690e47952d172612dfa3356e5
Karthick S <selvendrankarthick@gmail.com>
2020-01-18 15:57:41 +0530

    Updates

---
+++ precache-manifest.86cb1fc9d4f2a44931a86116daed4d95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "396a4b0b98aa17728f0abb55592d7dcb",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "d3b1021e75ee3615ea6d",
+    "url": "/calculator/static/css/main.0f37d341.chunk.css"
+  },
+  {
+    "revision": "00f4cf5937533a5725f0",
+    "url": "/calculator/static/js/2.6f7f0289.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/calculator/static/js/2.6f7f0289.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d3b1021e75ee3615ea6d",
+    "url": "/calculator/static/js/main.fdd688d9.chunk.js"
+  },
+  {
+    "revision": "c3d9a3bbcb6bc1177d26",
+    "url": "/calculator/static/js/runtime-main.250539d0.js"
+  }
+]);
